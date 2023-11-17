8e1d8090ae6806d70bf5f597b8e31fb0af6c3b67
Zjinnnn <zongjin.2@gmail.com>
2023-11-17 23:12:52 +0800

    Update solutions
    Remarks:
    
    - Assignment 2

--- routes.py
+++ routes.py
@@ -1,4 +1,10 @@
 from flask import Blueprint, jsonify, request
+from pymongo import MongoClient
+
+# Connect to MongoDB
+cluster = MongoClient("mongodb+srv://devsbe:U9MJGWLZXS885FqS@cluster0.6capukj.mongodb.net/")
+db = cluster["db1"]
+collection = db["as2"]
 
 main = Blueprint('main', __name__)
 
@@ -6,13 +12,24 @@ main = Blueprint('main', __name__)
 def home():
     return "Hello Backend"
 
-@main.route('/about_me', methods=['POST'])
+
+@main.route('/about_me', methods=['GET','POST'])
 def about_me():
     try:
         if request.method == "POST":
             print("Received POST Request")
             data = request.get_json()
-            return jsonify(data), 200
+            collection.insert_one(data)
+            return jsonify({"Msg": "Data saved successfully"}), 201
+        if request.method == "GET":
+            name = request.args.get('name')
+            data = collection.find_one({"Name": name})
+            if data:
+                # Convert ObjectId to string
+                data['_id'] = str(data['_id'])
+                return jsonify(data), 200
+            else:
+                return jsonify({"msg": "Data not found"}), 404
     except Exception as e:
         print(f"Error: {e}")
-        return "Can't add", 404
+        return "Connection Error", 404
